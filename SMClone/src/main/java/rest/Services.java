e7e9b9e346d4df614b69f4fe668a4ed552e93a3f
Basil <basil.omsha@gmail.com>
2022-08-19 17:42:38 +0300

    Upload function
    The upload function has been created but not used yet.

--- SMClone/src/main/java/rest/Services.java
+++ SMClone/src/main/java/rest/Services.java
@@ -9,6 +9,8 @@ import javax.servlet.http.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+import org.glassfish.jersey.media.multipart.*;
+
 import dao.Dao;
 import model.User;
 import security.SecurityUtils;
@@ -39,7 +41,7 @@ public class Services {
 			@Context HttpServletResponse response) {
 		Dao.updateEmail(mvm, request, response);
 	}
-	
+
 	@POST
 	@Path("/updateGender")
 	@Consumes("application/x-www-form-urlencoded")
@@ -47,7 +49,7 @@ public class Services {
 			@Context HttpServletResponse response) {
 		Dao.updateGender(mvm, request, response);
 	}
-	
+
 	@POST
 	@Path("/updateBirthday")
 	@Consumes("application/x-www-form-urlencoded")
@@ -63,12 +65,37 @@ public class Services {
 			@Context HttpServletResponse response) {
 		Dao.updatingPassword(mvm, request, response);
 	}
-	
+
 	@POST
 	@Path("/deleteAccount")
 	@Consumes("application/x-www-form-urlencoded")
 	public void deleteAccount(MultivaluedMap<String, String> mvm, @Context HttpServletRequest request,
 			@Context HttpServletResponse response) {
 		Dao.deleteAccount(mvm, request, response);
+	}
+
+	@POST
+	@Path("/fileupload")
+	@Consumes({ MediaType.MULTIPART_FORM_DATA })
+	public Response uploadFile(@FormDataParam("file") InputStream fileInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileMetaData, @Context ServletContext sc)
+			throws Exception {
+//		Dao.uploadFile(fileInputStream, fileMetaData, sc);
+		String UPLOAD_PATH = sc.getRealPath("/");
+		try {
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + "/" + fileMetaData.getFileName()));
+			while ((read = fileInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+
+		} catch (IOException e) {
+			throw new WebApplicationException("Error while uploading file. Please try again !!");
+		}
+		return Response.ok("Data uploaded successfully !!").build();
 	}
 }
